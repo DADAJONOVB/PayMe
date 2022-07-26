ab56f857fbe6e7ba235f47a77410d513ddb9632e
DADAJONOVB <dadajonovboburjon3@gmail.com>
2022-07-26 03:54:20 +0300

    test project

---
+++ payment/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import TestView
+
+
+urlpatterns = [
+    path('paycom/', TestView.as_view())
+]
---
+++ payment/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+
+from paycomuz.views import MerchantAPIView
+from paycomuz import Paycom
+
+class CheckOrder(Paycom):
+    def check_order(self, amount, account, *args, **kwargs):
+        return self.ORDER_FOUND
+        
+    def successfully_payment(self, account, transaction, *args, **kwargs):
+        print(account)
+
+    def cancel_payment(self, account, transaction, *args, **kwargs):
+        print(account)
+      
+
+class TestView(MerchantAPIView):
+    VALIDATE_CLASS = CheckOrder
